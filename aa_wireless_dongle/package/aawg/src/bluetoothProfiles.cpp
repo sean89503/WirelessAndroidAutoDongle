43f83561077b9831a717756e4ce0fa505b99a1dc
Sean <sean9@me.com>
2026-04-04 20:28:07 -0700

    Refactor BluetoothHandler and add advertisement support
    Improved stability during rapid connection/disconnection cycles.

--- aa_wireless_dongle/package/aawg/src/bluetoothProfiles.cpp
+++ aa_wireless_dongle/package/aawg/src/bluetoothProfiles.cpp
@@ -1,204 +1,299 @@
 #include <stdio.h>
-#include <thread>
-#include <unistd.h>
-#include <fcntl.h>
-#include <arpa/inet.h>
 
 #include "common.h"
 #include "bluetoothHandler.h"
 #include "bluetoothProfiles.h"
+#include "bluetoothAdvertisement.h"
 
-#include <google/protobuf/message_lite.h>
-#include "proto/WifiStartRequest.pb.h"
-#include "proto/WifiInfoResponse.pb.h"
+static constexpr const char* ADAPTER_ALIAS_PREFIX = "WirelessAADongle-";
+static constexpr const char* ADAPTER_ALIAS_DONGLE_PREFIX = "AndroidAuto-Dongle-";
 
-static constexpr const char* INTERFACE_BLUEZ_PROFILE = "org.bluez.Profile1";
+static constexpr const char* BLUEZ_BUS_NAME = "org.bluez";
+static constexpr const char* BLUEZ_ROOT_OBJECT_PATH = "/";
+static constexpr const char* BLUEZ_OBJECT_PATH = "/org/bluez";
+
+static constexpr const char* INTERFACE_BLUEZ_ADAPTER = "org.bluez.Adapter1";
+static constexpr const char* INTERFACE_BLUEZ_LE_ADVERTISING_MANAGER = "org.bluez.LEAdvertisingManager1";
+
+static constexpr const char* INTERFACE_BLUEZ_DEVICE = "org.bluez.Device1";
+static constexpr const char* INTERFACE_BLUEZ_PROFILE_MANAGER = "org.bluez.ProfileManager1";
+
+static constexpr const char* LE_ADVERTISEMENT_OBJECT_PATH = "/com/aawgd/bluetooth/advertisement";
+
+static constexpr const char* AAWG_PROFILE_OBJECT_PATH = "/com/aawgd/bluetooth/aawg";
+static constexpr const char* AAWG_PROFILE_UUID = "4de17a00-52cb-11e6-bdf4-0800200c9a66";
+
+static constexpr const char* HSP_HS_PROFILE_OBJECT_PATH = "/com/aawgd/bluetooth/hsp";
+static constexpr const char* HSP_AG_UUID = "00001112-0000-1000-8000-00805f9b34fb";
+static constexpr const char* HSP_HS_UUID = "00001108-0000-1000-8000-00805f9b34fb";
 
 
-#pragma region BluezProfile
-BluezProfile::BluezProfile(DBus::Path path): DBus::Object(path) {
-    this->create_method<void(void)>(INTERFACE_BLUEZ_PROFILE, "Release", sigc::mem_fun(*this, &BluezProfile::Release));
-    this->create_method<void(DBus::Path, std::shared_ptr<DBus::FileDescriptor>, DBus::Properties)>(INTERFACE_BLUEZ_PROFILE ,"NewConnection", sigc::mem_fun(*this, &BluezProfile::NewConnection));
-    this->create_method<void(DBus::Path)>(INTERFACE_BLUEZ_PROFILE, "RequestDisconnection", sigc::mem_fun(*this, &BluezProfile::RequestDisconnection));
-}
-#pragma endregion BluezProfile
+class BluezAdapterProxy: private DBus::ObjectProxy {
+    BluezAdapterProxy(std::shared_ptr<DBus::Connection> conn, DBus::Path path): DBus::ObjectProxy(conn, BLUEZ_BUS_NAME, path) {
+        alias = this->create_property<std::string>(INTERFACE_BLUEZ_ADAPTER, "Alias");
+        powered = this->create_property<bool>(INTERFACE_BLUEZ_ADAPTER, "Powered");
+        discoverable = this->create_property<bool>(INTERFACE_BLUEZ_ADAPTER, "Discoverable");
+        pairable = this->create_property<bool>(INTERFACE_BLUEZ_ADAPTER, "Pairable");
 
+        registerAdvertisement = this->create_method<void(DBus::Path, DBus::Properties)>(INTERFACE_BLUEZ_LE_ADVERTISING_MANAGER, "RegisterAdvertisement");
+        unregisterAdvertisement = this->create_method<void(DBus::Path)>(INTERFACE_BLUEZ_LE_ADVERTISING_MANAGER, "UnregisterAdvertisement");
+    }
 
-#pragma region AAWirelessLauncher
-class AAWirelessLauncher {
 public:
-    AAWirelessLauncher(int fd): m_fd(fd) {};
-
-    void launch() {
-        // Make fd blocking
-        int fd_flags = fcntl(m_fd, F_GETFL);
-        fcntl(m_fd, F_SETFL, fd_flags & ~O_NONBLOCK);
-
-        WifiInfo wifiInfo = Config::instance()->getWifiInfo();
-
-        Logger::instance()->info("Sending WifiStartRequest (ip: %s, port: %d)\n", wifiInfo.ipAddress.c_str(), wifiInfo.port);
-        WifiStartRequest wifiStartRequest;
-        wifiStartRequest.set_ip_address(wifiInfo.ipAddress);
-        wifiStartRequest.set_port(wifiInfo.port);
-
-        SendMessage(MessageId::WifiStartRequest, &wifiStartRequest);
-
-        MessageId messageId = ReadMessage();
-
-        if (messageId != MessageId::WifiInfoRequest) {
-            Logger::instance()->info("Expected WifiInfoRequest, got %s (%d). Abort.\n", MessageName(messageId), messageId);
-            return;
-        }
-
-        Logger::instance()->info("Sending WifiInfoResponse (ssid: %s, bssid: %s)\n", wifiInfo.ssid.c_str(), wifiInfo.bssid.c_str());
-        WifiInfoResponse wifiInfoResponse;
-        wifiInfoResponse.set_ssid(wifiInfo.ssid);
-        wifiInfoResponse.set_key(wifiInfo.key);
-        wifiInfoResponse.set_bssid(wifiInfo.bssid);
-        wifiInfoResponse.set_security_mode(wifiInfo.securityMode);
-        wifiInfoResponse.set_access_point_type(wifiInfo.accessPointType);
-
-        SendMessage(MessageId::WifiInfoResponse, &wifiInfoResponse);
-
-        ReadMessage();
-        ReadMessage();
+    static std::shared_ptr<BluezAdapterProxy> create(std::shared_ptr<DBus::Connection> conn, DBus::Path path)
+    {
+      return std::shared_ptr<BluezAdapterProxy>(new BluezAdapterProxy(conn, path));
     }
 
-private:
-    enum class MessageId {
-        Invalid = -1,
-        WifiStartRequest = 1,
-        WifiInfoRequest = 2,
-        WifiInfoResponse = 3,
-        WifiVersionRequest = 4,
-        WifiVersionResponse = 5,
-        WifiConnectStatus = 6,
-        WifiStartResponse = 7,
-    };
-    std::string MessageName(MessageId messageId) {
-        switch (messageId) {
-            case MessageId::WifiStartRequest:
-                return "WifiStartRequest";
-            case MessageId::WifiInfoRequest:
-                return "WifiInfoRequest";
-            case MessageId::WifiInfoResponse:
-                return "WifiInfoResponse";
-            case MessageId::WifiVersionRequest:
-                return "WifiVersionRequest";
-            case MessageId::WifiVersionResponse:
-                return "WifiVersionResponse";
-            case MessageId::WifiConnectStatus:
-                return "WifiConnectStatus";
-            case MessageId::WifiStartResponse:
-                return "WifiStartResponse";
-            default:
-                return "UNKNOWN";
-        }
-    }
+    std::shared_ptr<DBus::PropertyProxy<std::string>> alias;
+    std::shared_ptr<DBus::PropertyProxy<bool>> powered;
+    std::shared_ptr<DBus::PropertyProxy<bool>> discoverable;
+    std::shared_ptr<DBus::PropertyProxy<bool>> pairable;
 
-    void SendMessage(MessageId messageId, google::protobuf::MessageLite* message) {
-        uint16_t messageSize = (uint16_t)message->ByteSizeLong();
-        uint16_t length = messageSize + 4;
-
-        unsigned char* buffer = new unsigned char[length];
-
-        uint16_t networkShort = 0;
-        networkShort = htons(messageSize);
-        memcpy(buffer, &networkShort, sizeof(networkShort));
-
-        networkShort = htons(static_cast<uint16_t>(messageId));
-        memcpy(buffer + 2, &networkShort, sizeof(networkShort));
-
-        message->SerializeToArray(buffer + 4, messageSize);
-
-        ssize_t wrote = write(m_fd, buffer, length);
-        if (wrote < 0) {
-            Logger::instance()->info("Error sending %s, messageId: %d\n", MessageName(messageId).c_str(), messageId);
-        }
-        else {
-            Logger::instance()->info("Sent %s, messageId: %d, wrote %d bytes\n", MessageName(messageId).c_str(), messageId, wrote);
-        }
-
-        delete[] buffer;
-    }
-
-    MessageId ReadMessage() {
-        uint16_t networkShort = 0;
-        ssize_t readBytes;
-
-        readBytes = read(m_fd, &networkShort, 2);
-        if (readBytes != 2) {
-            // Could not read 2 bytes. Do something.
-            Logger::instance()->info("Error reading length, read bytes: %d, errno: %s\n", readBytes, strerror(errno));
-            return MessageId::Invalid;
-        }
-        uint16_t length = ntohs(networkShort);
-
-        readBytes = read(m_fd, &networkShort, 2);
-        if (readBytes != 2) {
-            // Could not read 2 bytes. Do something.
-            Logger::instance()->info("Error reading message id, read bytes: %d, errno: %s\n", readBytes, strerror(errno));
-            return MessageId::Invalid;
-        }
-        MessageId messageId = static_cast<MessageId>(ntohs(networkShort));
-
-        Logger::instance()->info("Read %s. length: %d, messageId: %d\n", MessageName(messageId).c_str(), length, messageId);
-        
-        unsigned char* buffer = new unsigned char[length];
-        readBytes = read(m_fd, buffer, length);
-
-        delete[] buffer;
-
-        return messageId;
-    }
-
-    int m_fd;
+    std::shared_ptr<DBus::MethodProxy<void(DBus::Path, DBus::Properties)>> registerAdvertisement;
+    std::shared_ptr<DBus::MethodProxy<void(DBus::Path)>> unregisterAdvertisement;
 };
-#pragma endregion AAWirelessLauncher
 
-#pragma region AAWirelessProfile
-void AAWirelessProfile::Release() {
-    Logger::instance()->info("AA Wireless Release\n");
+
+BluetoothHandler& BluetoothHandler::instance() {
+    static BluetoothHandler instance;
+    return instance;
 }
 
-void AAWirelessProfile::NewConnection(DBus::Path path, std::shared_ptr<DBus::FileDescriptor> fd, DBus::Properties fdProperties) {
-    Logger::instance()->info("AA Wireless NewConnection\n");
-    Logger::instance()->info("Path: %s, fd: %d\n", path.c_str(), fd->descriptor());
+DBus::ManagedObjects BluetoothHandler::getBluezObjects() {
+    std::shared_ptr<DBus::ObjectProxy> m_bluezRootObject = m_connection->create_object_proxy(BLUEZ_BUS_NAME, BLUEZ_ROOT_OBJECT_PATH);
+    DBus::MethodProxy getManagedObjects = *(m_bluezRootObject->create_method<DBus::ManagedObjects(void)>("org.freedesktop.DBus.ObjectManager", "GetManagedObjects"));
 
-    AAWirelessLauncher(fd->descriptor()).launch();
-    Logger::instance()->info("Bluetooth launch sequence completed\n");
+    return getManagedObjects();
 }
 
-void AAWirelessProfile::RequestDisconnection(DBus::Path path) {
-    Logger::instance()->info("AA Wireless RequestDisconnection\n");
-    Logger::instance()->info("Path: %s\n", path.c_str());
+void BluetoothHandler::initAdapter() {
+    DBus::ManagedObjects objects = getBluezObjects();
+
+    std::string adapter_path;
+    for (auto const& [path, interfaces]: objects) {
+        for (auto const& [interface, properties]: interfaces) {
+            if (interface == INTERFACE_BLUEZ_ADAPTER) {
+                adapter_path = path;
+                Logger::instance()->info("Using bluetooth adapter at path: %s\n", path.c_str());
+                break;
+            }
+        }
+        if (!adapter_path.empty()) {
+            break;
+        }
+    }
+
+    if (adapter_path.empty()) {
+        Logger::instance()->info("Did not find any bluetooth adapters\n");
+    }
+    else {
+        m_adapter = BluezAdapterProxy::create(m_connection, adapter_path);
+        m_adapter->alias->set_value(m_adapterAlias);
+        Logger::instance()->info("Bluetooth adapter alias: %s\n", m_adapterAlias.c_str());
+    }
 }
 
-AAWirelessProfile::AAWirelessProfile(DBus::Path path): BluezProfile(path) {};
+void BluetoothHandler::setPower(bool on) {
+    if (!m_adapter) {
+        return;
+    }
 
-/* static */ std::shared_ptr<AAWirelessProfile> AAWirelessProfile::create(DBus::Path path) {
-    return std::shared_ptr<AAWirelessProfile>(new AAWirelessProfile(path));
-}
-#pragma endregion AAWirelessProfile
-
-#pragma region HSPHSProfile
-void HSPHSProfile::Release() {
-    Logger::instance()->info("HSP HS Release\n");
+    m_adapter->powered->set_value(on);
+    Logger::instance()->info("Bluetooth adapter was powered %s\n", on ? "on" : "off");
 }
 
-void HSPHSProfile::NewConnection(DBus::Path path, std::shared_ptr<DBus::FileDescriptor> fd, DBus::Properties fdProperties) {
-    Logger::instance()->info("HSP HS NewConnection\n");
-    Logger::instance()->info("Path: %s, fd: %d\n", path.c_str(), fd->descriptor());
+void BluetoothHandler::setPairable(bool pairable) {
+    if (!m_adapter) {
+        return;
+    }
+
+    m_adapter->discoverable->set_value(pairable);
+    m_adapter->pairable->set_value(pairable);
+    Logger::instance()->info("Bluetooth adapter is now discoverable and pairable\n");
 }
 
-void HSPHSProfile::RequestDisconnection(DBus::Path path) {
-    Logger::instance()->info("HSP HS RequestDisconnection\n");
-    Logger::instance()->info("Path: %s\n", path.c_str());
+void BluetoothHandler::exportProfiles() {
+    std::shared_ptr<DBus::ObjectProxy> bluezObject = m_connection->create_object_proxy(BLUEZ_BUS_NAME, BLUEZ_OBJECT_PATH);
+    DBus::MethodProxy registerProfile = *(bluezObject->create_method<void(DBus::Path, std::string, DBus::Properties)>(INTERFACE_BLUEZ_PROFILE_MANAGER, "RegisterProfile"));
+
+    // Register AA Wireless Profile
+    m_aawProfile = AAWirelessProfile::create(AAWG_PROFILE_OBJECT_PATH);
+    if (m_connection->register_object(m_aawProfile, DBus::ThreadForCalling::DispatcherThread) != DBus::RegistrationStatus::Success) {
+        Logger::instance()->info("Failed to register AA Wireless profile\n");
+    }
+
+    registerProfile(AAWG_PROFILE_OBJECT_PATH, AAWG_PROFILE_UUID, {
+        {"Name", DBus::Variant("AA Wireless")},
+        {"Role", DBus::Variant("server")},
+        {"Channel", DBus::Variant(uint16_t(8))},
+    });
+    Logger::instance()->info("Bluetooth AA Wireless profile active\n");
+
+    if (Config::instance()->getConnectionStrategy() != ConnectionStrategy::DONGLE_MODE) {
+        // Register HSP Handset profile
+        m_hspProfile = HSPHSProfile::create(HSP_HS_PROFILE_OBJECT_PATH);
+        if (m_connection->register_object(m_hspProfile, DBus::ThreadForCalling::DispatcherThread) != DBus::RegistrationStatus::Success) {
+            Logger::instance()->info("Failed to register HSP Handset profile\n");
+        }
+        registerProfile(HSP_HS_PROFILE_OBJECT_PATH, HSP_HS_UUID, {
+            {"Name", DBus::Variant("HSP HS")},
+        });
+        Logger::instance()->info("HSP Handset profile active\n");
+    }
 }
 
-HSPHSProfile::HSPHSProfile(DBus::Path path): BluezProfile(path) {};
+void BluetoothHandler::startAdvertising() {
+    if (!m_adapter) {
+        return;
+    }
 
-/* static */ std::shared_ptr<HSPHSProfile> HSPHSProfile::create(DBus::Path path) {
-    return std::shared_ptr<HSPHSProfile>(new HSPHSProfile(path));
+    // Register Advertisement Object
+    m_leAdvertisement = BLEAdvertisement::create(LE_ADVERTISEMENT_OBJECT_PATH);
+
+    m_leAdvertisement->type->set_value("peripheral");
+    m_leAdvertisement->serviceUUIDs->set_value(std::vector<std::string>{AAWG_PROFILE_UUID});
+    m_leAdvertisement->localName->set_value(m_adapterAlias);
+
+    if (m_connection->register_object(m_leAdvertisement, DBus::ThreadForCalling::DispatcherThread) != DBus::RegistrationStatus::Success) {
+        Logger::instance()->info("Failed to register BLE Advertisement\n");
+    }
+
+    (*m_adapter->registerAdvertisement)(LE_ADVERTISEMENT_OBJECT_PATH, {});
+    Logger::instance()->info("BLE Advertisement started\n");
 }
-#pragma endregion HSPHSProfile
+
+void BluetoothHandler::stopAdvertising() {
+    if (!m_adapter) {
+        return;
+    }
+
+    (*m_adapter->unregisterAdvertisement)(LE_ADVERTISEMENT_OBJECT_PATH);
+    Logger::instance()->info("BLE Advertisement stopped\n");
+}
+
+void BluetoothHandler::connectDevice() {
+    DBus::ManagedObjects objects = getBluezObjects();
+
+    std::vector<std::string> device_paths;
+    for (auto const& [path, interfaces]: objects) {
+        for (auto const& [interface, properties]: interfaces) {
+            if (interface == INTERFACE_BLUEZ_DEVICE) {
+                device_paths.push_back(path);
+            }
+        }
+    }
+
+    if (!device_paths.size()) {
+        Logger::instance()->info("Did not find any connected bluetooth device\n");
+        return;
+    }
+
+    const bool isDongleMode = (Config::instance()->getConnectionStrategy() == ConnectionStrategy::DONGLE_MODE);
+
+    Logger::instance()->info("Found %d bluetooth devices\n", device_paths.size());
+
+    for (const std::string &device_path: device_paths) {
+        Logger::instance()->info("Trying to connect bluetooth device at path: %s\n", device_path.c_str());
+
+        std::shared_ptr<DBus::ObjectProxy> bluezDevice = m_connection->create_object_proxy(BLUEZ_BUS_NAME, device_path);
+        DBus::MethodProxy connectProfile = *(bluezDevice->create_method<void(std::string)>(INTERFACE_BLUEZ_DEVICE, "ConnectProfile"));
+        DBus::MethodProxy disconnect = *(bluezDevice->create_method<void()>(INTERFACE_BLUEZ_DEVICE, "Disconnect"));
+
+        std::shared_ptr<DBus::PropertyProxy<bool>> deviceConnected = bluezDevice->create_property<bool>(INTERFACE_BLUEZ_DEVICE, "Connected");
+
+        try {
+            if (deviceConnected) {
+                Logger::instance()->info("Bluetooth device already connected, disconnecting\n");
+                disconnect();
+            }
+            connectProfile(isDongleMode ? "" : HSP_AG_UUID);
+            Logger::instance()->info("Bluetooth connected to the device\n");
+            if (!isDongleMode) {
+                return;
+            }
+        } catch (DBus::Error& e) {
+            if (!isDongleMode) {
+                Logger::instance()->info("Failed to connect device at path: %s\n", device_path.c_str());
+            }
+        }
+    }
+
+    if (!isDongleMode) {
+        Logger::instance()->info("Failed to connect to any known bluetooth device\n");
+    }
+}
+
+void BluetoothHandler::retryConnectLoop() {
+    bool should_exit = false;
+    std::shared_ptr<std::promise<void>> promise = connectWithRetryPromise;
+    std::future<void> connectWithRetryFuture = promise->get_future();
+
+    while (!should_exit) {
+        connectDevice();
+
+        if (connectWithRetryFuture.wait_for(std::chrono::seconds(20)) == std::future_status::ready) {
+            should_exit = true;
+        }
+    }
+
+    if (Config::instance()->getConnectionStrategy() != ConnectionStrategy::DONGLE_MODE) {
+        BluetoothHandler::instance().powerOff();
+    }
+}
+
+void BluetoothHandler::init() {
+    // DBus::set_logging_function( DBus::log_std_err );
+    // DBus::set_log_level( SL_TRACE );
+
+    m_dispatcher = DBus::StandaloneDispatcher::create();
+    m_connection = m_dispatcher->create_connection( DBus::BusType::SYSTEM );
+
+    std::string adapterAliasPrefix = (Config::instance()->getConnectionStrategy() == ConnectionStrategy::DONGLE_MODE) ? ADAPTER_ALIAS_DONGLE_PREFIX : ADAPTER_ALIAS_PREFIX;
+
+    m_adapterAlias = adapterAliasPrefix + Config::instance()->getUniqueSuffix();
+
+    initAdapter();
+    exportProfiles();
+}
+
+void BluetoothHandler::powerOn() {
+    if (!m_adapter) {
+        return;
+    }
+
+    setPower(true);
+    setPairable(true);
+
+    if (Config::instance()->getConnectionStrategy() == ConnectionStrategy::DONGLE_MODE) {
+        startAdvertising();
+    }
+}
+
+std::optional<std::thread> BluetoothHandler::connectWithRetry() {
+    if (!m_adapter) {
+        return std::nullopt;
+    }
+
+    connectWithRetryPromise = std::make_shared<std::promise<void>>();
+    return std::thread(&BluetoothHandler::retryConnectLoop, this);
+}
+
+void BluetoothHandler::stopConnectWithRetry() {
+    if (connectWithRetryPromise) {
+        std::shared_ptr<std::promise<void>> promise = connectWithRetryPromise;
+        connectWithRetryPromise = nullptr;
+        promise->set_value();
+    }
+}
+
+void BluetoothHandler::powerOff() {
+    if (!m_adapter) {
+        return;
+    }
+
+    if (Config::instance()->getConnectionStrategy() == ConnectionStrategy::DONGLE_MODE) {
+        stopAdvertising();
+    }
+    setPower(false);
+}
