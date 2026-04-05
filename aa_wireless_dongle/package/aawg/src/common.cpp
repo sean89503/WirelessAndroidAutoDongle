1202e56620c791fd91bacfe9557404a06f69da50
Sean <sean9@me.com>
2026-04-04 19:17:09 -0700

    Refactor common.cpp to enhance Bluetooth handling
    Handshake Robustness: Bluetooth Timeout
      Added a 5-second timeout to the Bluetooth handshake socket in AAWirelessLauncher. This prevents the daemon from
      hanging indefinitely if a phone connects via Bluetooth but fails to complete the WiFi credential exchange.

--- aa_wireless_dongle/package/aawg/src/common.cpp
+++ aa_wireless_dongle/package/aawg/src/common.cpp
@@ -1,121 +1,212 @@
-#include <cstdlib>
-#include <cstdarg>
-#include <sstream>
-#include <fstream>
-#include <syslog.h>
+#include <stdio.h>
+#include <thread>
+#include <unistd.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
 
 #include "common.h"
+#include "bluetoothHandler.h"
+#include "bluetoothProfiles.h"
+
+#include <google/protobuf/message_lite.h>
+#include "proto/WifiStartRequest.pb.h"
 #include "proto/WifiInfoResponse.pb.h"
 
-#pragma region Config
-/*static*/ Config* Config::instance() {
-    static Config s_instance;
-    return &s_instance;
+static constexpr const char* INTERFACE_BLUEZ_PROFILE = "org.bluez.Profile1";
+
+
+#pragma region BluezProfile
+BluezProfile::BluezProfile(DBus::Path path): DBus::Object(path) {
+    this->create_method<void(void)>(INTERFACE_BLUEZ_PROFILE, "Release", sigc::mem_fun(*this, &BluezProfile::Release));
+    this->create_method<void(DBus::Path, std::shared_ptr<DBus::FileDescriptor>, DBus::Properties)>(INTERFACE_BLUEZ_PROFILE ,"NewConnection", sigc::mem_fun(*this, &BluezProfile::NewConnection));
+    this->create_method<void(DBus::Path)>(INTERFACE_BLUEZ_PROFILE, "RequestDisconnection", sigc::mem_fun(*this, &BluezProfile::RequestDisconnection));
 }
+#pragma endregion BluezProfile
 
-int32_t Config::getenv(std::string name, int32_t defaultValue) {
-    char* envValue = std::getenv(name.c_str());
-    try {
-        return envValue != nullptr ? std::stoi(envValue) : defaultValue;
-    }
-    catch(...) {
-        return defaultValue;
-    }
-}
 
-std::string Config::getenv(std::string name, std::string defaultValue) {
-    char* envValue = std::getenv(name.c_str());
-    return envValue != nullptr ? envValue : defaultValue;
-}
+#pragma region AAWirelessLauncher
+class AAWirelessLauncher {
+public:
+    AAWirelessLauncher(int fd): m_fd(fd) {};
 
-std::string Config::getMacAddress(std::string interface) {
-    std::ifstream addressFile("/sys/class/net/" + interface + "/address");
+    void launch() {
+        // Make fd blocking
+        int fd_flags = fcntl(m_fd, F_GETFL);
+        fcntl(m_fd, F_SETFL, fd_flags & ~O_NONBLOCK);
 
-    std::string macAddress;
-    getline(addressFile, macAddress);
-
-    return macAddress;
-}
-
-std::string Config::getUniqueSuffix() {
-    if (!uniqueSuffix.has_value()) {
-        std::string suffix = getenv("AAWG_UNIQUE_NAME_SUFFIX", "");
-        if (!suffix.empty()) {
-            uniqueSuffix = suffix;
-        } else {
-            std::ifstream serialNumberFile("/sys/firmware/devicetree/base/serial-number");
-
-            std::string serialNumber;
-            getline(serialNumberFile, serialNumber);
-
-            // Removing trailing null from serialNumber, pad at the beginning
-            serialNumber = std::string("00000000") + serialNumber.c_str();
-
-            uniqueSuffix = serialNumber.substr(serialNumber.size() - 6);
+        // Set a timeout for the socket
+        struct timeval tv;
+        tv.tv_sec = 5;
+        tv.tv_usec = 0;
+        if (setsockopt(m_fd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
+            Logger::instance()->info("Error setting timeout on bluetooth socket: %s\n", strerror(errno));
         }
+
+        WifiInfo wifiInfo = Config::instance()->getWifiInfo();
+
+        Logger::instance()->info("Sending WifiStartRequest (ip: %s, port: %d)\n", wifiInfo.ipAddress.c_str(), wifiInfo.port);
+        WifiStartRequest wifiStartRequest;
+        wifiStartRequest.set_ip_address(wifiInfo.ipAddress);
+        wifiStartRequest.set_port(wifiInfo.port);
+
+        SendMessage(MessageId::WifiStartRequest, &wifiStartRequest);
+
+        MessageId messageId = ReadMessage();
+
+        if (messageId != MessageId::WifiInfoRequest) {
+            Logger::instance()->info("Expected WifiInfoRequest, got %s (%d). Abort.\n", MessageName(messageId), messageId);
+            return;
+        }
+
+        Logger::instance()->info("Sending WifiInfoResponse (ssid: %s, bssid: %s)\n", wifiInfo.ssid.c_str(), wifiInfo.bssid.c_str());
+        WifiInfoResponse wifiInfoResponse;
+        wifiInfoResponse.set_ssid(wifiInfo.ssid);
+        wifiInfoResponse.set_key(wifiInfo.key);
+        wifiInfoResponse.set_bssid(wifiInfo.bssid);
+        wifiInfoResponse.set_security_mode(wifiInfo.securityMode);
+        wifiInfoResponse.set_access_point_type(wifiInfo.accessPointType);
+
+        SendMessage(MessageId::WifiInfoResponse, &wifiInfoResponse);
+
+        ReadMessage();
+        ReadMessage();
     }
 
-    return uniqueSuffix.value();
-}
-
-WifiInfo Config::getWifiInfo() {
-    if (!wifiInfo.has_value()) {
-        wifiInfo = {
-            getenv("AAWG_WIFI_SSID", "AAWirelessDongle"),
-            getenv("AAWG_WIFI_PASSWORD", "ConnectAAWirelessDongle"),
-            getenv("AAWG_WIFI_BSSID", getMacAddress("wlan0")),
-            SecurityMode::WPA2_PERSONAL,
-            AccessPointType::DYNAMIC,
-            getenv("AAWG_PROXY_IP_ADDRESS", "10.0.0.1"),
-            getenv("AAWG_PROXY_PORT", 5288),
-        };
-    }
-
-    return wifiInfo.value();
-}
-
-ConnectionStrategy Config::getConnectionStrategy() {
-    if (!connectionStrategy.has_value()) {
-        const int32_t connectionStrategyEnv = getenv("AAWG_CONNECTION_STRATEGY", 1);
-
-        switch (connectionStrategyEnv) {
-            case 0:
-                connectionStrategy = ConnectionStrategy::DONGLE_MODE;
-                break;
-            case 1:
-                connectionStrategy = ConnectionStrategy::PHONE_FIRST;
-                break;
-            case 2:
-                connectionStrategy = ConnectionStrategy::USB_FIRST;
-                break;
+private:
+    enum class MessageId {
+        Invalid = -1,
+        WifiStartRequest = 1,
+        WifiInfoRequest = 2,
+        WifiInfoResponse = 3,
+        WifiVersionRequest = 4,
+        WifiVersionResponse = 5,
+        WifiConnectStatus = 6,
+        WifiStartResponse = 7,
+    };
+    std::string MessageName(MessageId messageId) {
+        switch (messageId) {
+            case MessageId::WifiStartRequest:
+                return "WifiStartRequest";
+            case MessageId::WifiInfoRequest:
+                return "WifiInfoRequest";
+            case MessageId::WifiInfoResponse:
+                return "WifiInfoResponse";
+            case MessageId::WifiVersionRequest:
+                return "WifiVersionRequest";
+            case MessageId::WifiVersionResponse:
+                return "WifiVersionResponse";
+            case MessageId::WifiConnectStatus:
+                return "WifiConnectStatus";
+            case MessageId::WifiStartResponse:
+                return "WifiStartResponse";
             default:
-                connectionStrategy = ConnectionStrategy::PHONE_FIRST;
-                break;
+                return "UNKNOWN";
         }
     }
 
-    return connectionStrategy.value();
-}
-#pragma endregion Config
+    void SendMessage(MessageId messageId, google::protobuf::MessageLite* message) {
+        uint16_t messageSize = (uint16_t)message->ByteSizeLong();
+        uint16_t length = messageSize + 4;
 
-#pragma region Logger
-/*static*/ Logger* Logger::instance() {
-    static Logger s_instance;
-    return &s_instance;
+        unsigned char* buffer = new unsigned char[length];
+
+        uint16_t networkShort = 0;
+        networkShort = htons(messageSize);
+        memcpy(buffer, &networkShort, sizeof(networkShort));
+
+        networkShort = htons(static_cast<uint16_t>(messageId));
+        memcpy(buffer + 2, &networkShort, sizeof(networkShort));
+
+        message->SerializeToArray(buffer + 4, messageSize);
+
+        ssize_t wrote = write(m_fd, buffer, length);
+        if (wrote < 0) {
+            Logger::instance()->info("Error sending %s, messageId: %d\n", MessageName(messageId).c_str(), messageId);
+        }
+        else {
+            Logger::instance()->info("Sent %s, messageId: %d, wrote %d bytes\n", MessageName(messageId).c_str(), messageId, wrote);
+        }
+
+        delete[] buffer;
+    }
+
+    MessageId ReadMessage() {
+        uint16_t networkShort = 0;
+        ssize_t readBytes;
+
+        readBytes = read(m_fd, &networkShort, 2);
+        if (readBytes != 2) {
+            // Could not read 2 bytes. Do something.
+            Logger::instance()->info("Error reading length, read bytes: %d, errno: %s\n", readBytes, strerror(errno));
+            return MessageId::Invalid;
+        }
+        uint16_t length = ntohs(networkShort);
+
+        readBytes = read(m_fd, &networkShort, 2);
+        if (readBytes != 2) {
+            // Could not read 2 bytes. Do something.
+            Logger::instance()->info("Error reading message id, read bytes: %d, errno: %s\n", readBytes, strerror(errno));
+            return MessageId::Invalid;
+        }
+        MessageId messageId = static_cast<MessageId>(ntohs(networkShort));
+
+        Logger::instance()->info("Read %s. length: %d, messageId: %d\n", MessageName(messageId).c_str(), length, messageId);
+        
+        unsigned char* buffer = new unsigned char[length];
+        readBytes = read(m_fd, buffer, length);
+
+        delete[] buffer;
+
+        return messageId;
+    }
+
+    int m_fd;
+};
+#pragma endregion AAWirelessLauncher
+
+#pragma region AAWirelessProfile
+void AAWirelessProfile::Release() {
+    Logger::instance()->info("AA Wireless Release\n");
 }
 
-Logger::Logger() {
-    openlog(nullptr, LOG_PERROR | LOG_PID, LOG_USER);
+void AAWirelessProfile::NewConnection(DBus::Path path, std::shared_ptr<DBus::FileDescriptor> fd, DBus::Properties fdProperties) {
+    Logger::instance()->info("AA Wireless NewConnection\n");
+    Logger::instance()->info("Path: %s, fd: %d\n", path.c_str(), fd->descriptor());
+
+    AAWirelessLauncher(fd->descriptor()).launch();
+    Logger::instance()->info("Bluetooth launch sequence completed\n");
 }
 
-Logger::~Logger() {
-    closelog();
+void AAWirelessProfile::RequestDisconnection(DBus::Path path) {
+    Logger::instance()->info("AA Wireless RequestDisconnection\n");
+    Logger::instance()->info("Path: %s\n", path.c_str());
 }
 
-void Logger::info(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
-    vsyslog(LOG_INFO, format, args);
-    va_end(args);
+AAWirelessProfile::AAWirelessProfile(DBus::Path path): BluezProfile(path) {};
+
+/* static */ std::shared_ptr<AAWirelessProfile> AAWirelessProfile::create(DBus::Path path) {
+    return std::shared_ptr<AAWirelessProfile>(new AAWirelessProfile(path));
 }
-#pragma endregion Logger
+#pragma endregion AAWirelessProfile
+
+#pragma region HSPHSProfile
+void HSPHSProfile::Release() {
+    Logger::instance()->info("HSP HS Release\n");
+}
+
+void HSPHSProfile::NewConnection(DBus::Path path, std::shared_ptr<DBus::FileDescriptor> fd, DBus::Properties fdProperties) {
+    Logger::instance()->info("HSP HS NewConnection\n");
+    Logger::instance()->info("Path: %s, fd: %d\n", path.c_str(), fd->descriptor());
+}
+
+void HSPHSProfile::RequestDisconnection(DBus::Path path) {
+    Logger::instance()->info("HSP HS RequestDisconnection\n");
+    Logger::instance()->info("Path: %s\n", path.c_str());
+}
+
+HSPHSProfile::HSPHSProfile(DBus::Path path): BluezProfile(path) {};
+
+/* static */ std::shared_ptr<HSPHSProfile> HSPHSProfile::create(DBus::Path path) {
+    return std::shared_ptr<HSPHSProfile>(new HSPHSProfile(path));
+}
+#pragma endregion HSPHSProfile
